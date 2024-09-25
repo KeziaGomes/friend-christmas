178288cad68a6b6499cc7a68b23a7e1056eccf07
kezia <keziagomes994@gmail.com>
2024-09-25 17:27:26 -0400

    feat: create entities

--- database/migrations/2024_09_25_211309_create_participants_table.php
+++ database/migrations/2024_09_25_211309_create_participants_table.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use function Psy\debug;
-
 return new class extends Migration
 {
     /**
@@ -13,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('groups', function (Blueprint $table) {
+        Schema::create('participants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('participants');
     }
 };
